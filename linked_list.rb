df876a381fd92b5615c6656c05937a506f6c5d81
lujanfernaud <lujanfernaud@gmail.com>
2017-04-26 23:47:38 +0100

    Start implementation

---
+++ linked_list.rb
@@ -0,0 +1,9 @@
+# Project: Linked list in Ruby for The Odin Project
+# Author: Luján Fernaud
+# Url: http://www.theodinproject.com/courses/ruby-programming/lessons/linked-lists
+
+class LinkedList
+end
+
+class Node
+end
